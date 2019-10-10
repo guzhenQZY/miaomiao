c961b7bd0822538778b201ca8f44abb3da6dfc80
guzhenQZY <2974179299@qq.com>
2019-10-10 16:10:32 +0800

    add createComponents

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,19 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+    <keep-alive>
+        <transition>
+        <router-view/>
+        </transition>
+    </keep-alive>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+<style>
+.v-enter{
+opacity: 0;
 }
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+.v-enter-to{
+opacity: 1;
+}
+.v-enter-active{
+transition: all 2s ease;
 }
 </style>
---
+++ src/views/Mine/index.vue
@@ -0,0 +1,28 @@
+<template>
+    <div id="main">
+        <Header title="我的喵喵"></Header>
+            <div id="content">
+                <Login></Login>
+            </div>
+        <Tabbar></Tabbar>
+    </div>
+</template>
+
+<script>
+
+    import Header from '@/components/Header';
+    import Tabbar from '@/components/Tabbar';
+    import Login from '@/components/Login'
+    export default{
+        naame : 'Mine',
+        components : {
+            Header,
+            Tabbar,
+            Login
+        }
+    }
+</script>
+
+<style>
+
+</style>
